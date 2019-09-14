1f63bad645c7a7194ca5862998f6aae059219525
phelipp <phelippnd@gmail.com>
2019-09-14 09:30:25 -0300

    Swap

--- app/src/main/java/pndtech/com/istarve/View/cartContainer/CartContainerFragment.java
+++ app/src/main/java/pndtech/com/istarve/View/cartContainer/CartContainerFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
